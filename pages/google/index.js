2e692ce3a7cb196507b3b5d1c44ef673d623b858
Pariwat <pariwat@theiconweb.com>
2020-01-08 22:59:21 +0700

    Google Auth

--- pages/google/index.js
+++ pages/google/index.js
@@ -1,18 +1,48 @@
+import React, { Component } from 'react';
 import Layout from '../../components/layout';
 import Menu from '../../components/menu';
 import { auth, firebase } from '../../src/firebase'
 
-function GoogleAuth() {
+class GoogleAuth extends Component {
 
-    const provider = new firebase.auth.GoogleAuthProvider();    
+    constructor() {
+        super();
+        let provider = new firebase.auth.GoogleAuthProvider();
+        this.state = {
+            provider: provider,
+            photo:'',
+            displayname: ''
+        };
+        this.initPage();
+        this.signinGoogle = this.signinGoogle.bind(this);
+        this.logoutGoogle = this.logoutGoogle.bind(this);
+    }
     
-    function signinGoogle(){
-        firebase.auth().signInWithPopup(provider).then(function(result) {
+        
+    initPage(){
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                this.setState({
+                    ... this.state,
+                    photo: user.photoURL,
+                    displayname: user.displayName
+                });
+            }else{
+
+            }
+        });
+    }
+    
+    
+    signinGoogle(){
+        const current = this;
+        firebase.auth().signInWithPopup(this.state.provider).then(function(result) {
         // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-            const token = result.credential.accessToken;
-            const user = result.user;
-            console.log(token);
-            console.log(user);
+            current.setState({
+                ... current.state,
+                photo: result.user.photoURL,
+                displayname: result.user.displayName
+            });
         }).catch(function(error) {
         // Handle Errors here.
             const errorCode = error.code;
@@ -26,9 +56,15 @@ function GoogleAuth() {
         });
     }
 
-    function logoutGoogle(){
+    logoutGoogle(){
+        const current = this;
         firebase.auth().signOut().then(function() {
             // Sign-out successful.
+            current.setState({
+                ... current.state,
+                photo: '',
+                displayname: ''
+            });
             console.log('Sign-out successful.');
         }).catch(function(error) {
         // An error happened.
@@ -36,18 +72,26 @@ function GoogleAuth() {
         });
     }
 
-    return (
-        <>
-        <Menu />
-        <Layout>
-            <h1>Firebase Google Auth</h1>
-            <div>
-                <button id="signin" name="signin" onClick={signinGoogle}>Sign In</button>
-                <button id="logout" name="logout" onClick={logoutGoogle}>Log out</button>
-            </div>
-        </Layout>
-        </>
-    );
+    render(){
+        return (
+            <>
+            <Menu />
+            <Layout>
+                <h1>Firebase Google Auth</h1>
+                <div>{this.state.displayname}</div>
+                <div>
+                    <img src={this.state.photo} ></img>
+                </div>
+                <div>
+                {this.state.displayname =='' && (
+                    <button id="signin" name="signin" onClick={this.signinGoogle}>Sign In</button>
+                )}
+                    <button id="logout" name="logout" onClick={this.logoutGoogle}>Log out</button>
+                </div>
+            </Layout>
+            </>
+        );
+    }
 }
   
 export default GoogleAuth
