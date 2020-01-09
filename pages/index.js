39bf6608d2635a45d29547361dc5b932e3914d11
Pariwat <pariwat@theiconweb.com>
2020-01-09 23:09:00 +0700

    Email Auth

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,6 @@ class FacebookAuth extends Component {
     initPage(){
         firebase.auth().onAuthStateChanged((user) => {
             if(user){
-                // const displayname = user.displayname;
-                // const email = user.email;
-                // const emailVerified = user.emailVerified;
-                // const photoURL = user.photoURL;
-                // const isAnonymous = user.isAnonymous;
-                // const uid = user.uid;
-                // const providerData = user.providerData;
                 this.setState({
                     ... this.state,
                     photo: user.photoURL,
